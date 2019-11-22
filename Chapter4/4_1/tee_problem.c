3c6518fcb8281d18f760f1cb5a794de23234df6e
Brandon Givens <brandon.a.givens@gmail.com>
2019-11-21 20:52:46 -0700

    Add command line parsing Add ability to open/create files passed in through command line

--- Chapter4/4_1/tee_problem.c
+++ Chapter4/4_1/tee_problem.c
@@ -1,12 +1,42 @@
 #include <stdio.h>
-int main()
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[])
 {
-	while(1)
-	{
-		char str[256];
-		scanf("%s", str);
-		printf("%s\n", str);
+	int append_flag = 0;
+	int option;
+	extern int optind;
+
+	while(( option = getopt(argc, argv, "a")) != -1){
+		switch (option){
+		case 'a':
+			append_flag=1;
+			break;
+		default:
+			fprintf(stderr, "Usage: %s [-a append] filenames...",
+				       	argv[0]);
+			exit(EXIT_FAILURE);
+		}
 	}
-	printf("Hello, World!");
+
+	int fds[argc];
+	int num_files = argc-optind;
+	for(int i = optind ; i < argc; i++){
+		fds[i-optind] = open(argv[i],
+				O_RDWR | O_CREAT | O_TRUNC,
+				S_IRUSR | S_IWUSR);
+		printf("File name: %s, File Descriptor: %d\n", 
+				argv[i],
+				fds[i-optind]);
+	}
+
+	//while(1){
+	//	char str[256];
+	//	scanf("%s", str);
+	//	printf("%s\n", str);
+	//}
 	return 0;
 }
